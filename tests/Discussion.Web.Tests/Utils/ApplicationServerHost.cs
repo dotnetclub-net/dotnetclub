ff02835a061cb18be10309c999f1c977811f1cc3
Jijie Chen <jijie.chen@outlook.com>
2015-10-24 09:11:46 +0800

    added a base class to hold initialize test env

---
+++ tests/Discussion.Web.Tests/Utils/ApplicationServerHost.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Discussion.Web.Tests
+{
+    public class ApplicationServerHost: IDisposable
+    {
+        static ApplicationServerHost()
+        {
+            // bootstrap
+        }
+
+        public ApplicationServerHost()
+        {
+            // initialize every time
+        }
+
+
+        void IDisposable.Dispose()
+        {
+            // recyle resources
+        }
+    }
+
+}
