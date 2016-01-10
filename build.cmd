e1446908fc4456bad4c9f835c63a14028f89f5c9
Jijie Chen <jijie.chen@someplace.com>
2016-01-10 23:37:07 +0800

    added basic build support files

---
+++ build.cmd
@@ -0,0 +1,23 @@
+@echo off
+cd %~dp0
+
+SETLOCAL
+SET BUILDCMD_DNX_VERSION=1.0.0-rc1-update1
+
+IF EXIST .nuget\nuget.exe goto build
+md .nuget
+copy build-tools\nuget.exe .nuget\nuget.exe > nul
+
+
+:build
+IF "%BUILDCMD_DNX_VERSION%"=="" (
+    SET BUILDCMD_DNX_VERSION=latest
+)
+IF "%SKIP_DNX_INSTALL%"=="" (
+    rem CALL build-tools\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CoreCLR -arch x86 -alias default
+    CALL build-tools\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x86 -alias default
+) ELSE (
+    CALL build-tools\KoreBuild\build\dnvm use default -runtime CLR -arch x86
+)
+
+build-tools\Sake\tools\Sake.exe -I build-tools\KoreBuild\build -f makefile.shade %*
