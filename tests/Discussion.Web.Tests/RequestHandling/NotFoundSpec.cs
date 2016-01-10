124008e9baf531b7659b39af17a9055c2478f359
Jijie Chen <jijie.chen@someplace.com>
2016-01-10 21:58:57 +0800

    added sdk section in global.json; added some test helpers

---
+++ tests/Discussion.Web.Tests/RequestHandling/NotFoundSpec.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNet.Builder.Internal;
+using Microsoft.AspNet.Http;
+using Microsoft.AspNet.Http.Internal;
+using Microsoft.Framework.DependencyInjection;
+using Xunit;
+
+namespace Discussion.Web.Tests.RequestHandling
+{
+    public class NotFoundSpec
+    {
+        [Fact]
+        public async void should_response_not_found_by_default()
+        {
+            // arrage
+            var services = new ServiceCollection();
+            var startup = new Web.Startup();
+            startup.ConfigureServices(services);
+
+            var serviceProvider = services.BuildServiceProvider();
+            var app = new ApplicationBuilder(serviceProvider);
+            startup.Configure(app);
+
+
+            // act
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.Path = "/";
+
+            // assert
+            httpContext.Response.StatusCode.ShouldEqual(200);
+
+
+
+            // act
+            await app.Build().Invoke(httpContext);
+
+            // assert
+            httpContext.Response.StatusCode.ShouldEqual(404);
+            // httpContext.Response.Body
+        }
+    }
+}
---
+++ tests/Discussion.Web.Tests/Utils/HttpRequestBuilder.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNet.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discussion.Web.Tests.Utils
+{
+    public class HttpRequestBuilder
+    {
+
+        public HttpRequestBuilder WithPath(string path)
+        {
+            return this;
+        }
+
+        public HttpRequestBuilder WithQuery(string query)
+        {
+            return this;
+        }
+
+
+        public HttpRequestBuilder WithHttpMethod(string httpMethod)
+        {
+            return this;
+        }
+
+        public HttpRequestBuilder WithHeader(string name, string value)
+        {
+            return this;
+        }
+
+        public HttpRequestBuilder WithCookie(string name, string value)
+        {
+            return this;
+        }
+
+
+        public HttpRequestBuilder WithCookies(IDictionary<string, string> cookies)
+        {
+            return this;
+        }
+
+
+        public HttpRequestBuilder WithFile(string filePath)
+        {
+            return this;
+        }
+
+        public HttpRequestBuilder WithFile(Stream fileContent)
+        {
+            return this;
+        }
+
+
+        public HttpRequestBuilder WithContent(string content)
+        {
+            return this;
+        }
+
+        public HttpRequestBuilder WithContent(Stream uploadContent)
+        {
+            return this;
+        }
+
+        public HttpRequestBuilder WithContentType(string name, string value)
+        {
+            return this;
+        }
+
+        public HttpRequestBuilder WithWebForm(string name, string value)
+        {
+            return this;
+        }
+
+
+        public HttpContext SendRequest()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
