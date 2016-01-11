fe0995032b400c4d11a01c1be0c84ea279b561f6
Jijie Chen <jijie.chen@someplace.com>
2016-01-12 02:21:06 +0800

    fixed sake to be the entry point of everything

--- sake.cmd
+++ sake.cmd
@@ -1,24 +1,4 @@
 @echo off
 cd %~dp0
 
-SETLOCAL
-SET DNX_FEED=https://www.nuget.org/api/v2
-SET BUILDCMD_DNX_VERSION=1.0.0-rc1-update1
-
-IF EXIST .nuget\nuget.exe goto build
-md .nuget
-copy build-tools\nuget.exe .nuget\nuget.exe > nul
-
-
-:build
-IF "%BUILDCMD_DNX_VERSION%"=="" (
-    SET BUILDCMD_DNX_VERSION=latest
-)
-IF "%SKIP_DNX_INSTALL%"=="" (
-    rem CALL build-tools\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CoreCLR -arch x86 -alias default
-    CALL build-tools\KoreBuild\build\dnvm install %BUILDCMD_DNX_VERSION% -runtime CLR -arch x86 -alias default
-) ELSE (
-    CALL build-tools\KoreBuild\build\dnvm use default -runtime CLR -arch x86
-)
-
-build-tools\Sake\tools\Sake.exe -I build-tools\KoreBuild\build -f makefile.shade %*
+.\build-tools\Sake\tools\Sake.exe %*
--- sake.sh
+++ sake.sh
@@ -1,23 +1,3 @@
 #!/usr/bin/env bash
-export DNX_FEED=https://www.nuget.org/api/v2
-build_dnx_version=1.0.0-rc1-update1
 
-if test ! -f .nuget/nuget.exe; then
-	if test ! -e .nuget; then
-		mkdir .nuget
-	fi
-    cp build-tools/nuget.exe .nuget/nuget.exe
-fi
-
-if ! type dnvm > /dev/null 2>&1; then
-    source build-tools/KoreBuild/build/dnvm.sh
-fi
-
-if ! type dnx > /dev/null 2>&1 || [ -z "$SKIP_DNX_INSTALL" ]; then
-    dnvm install %build_dnx_version -runtime coreclr -alias default
-    dnvm install %build_dnx_version -runtime mono -alias default
-else
-    dnvm use default -runtime mono
-fi
-
-mono build-tools/Sake/tools/Sake.exe -I build-tools/KoreBuild/build -f makefile.shade "$@"
+mono build-tools/Sake/tools/Sake.exe "$@"
