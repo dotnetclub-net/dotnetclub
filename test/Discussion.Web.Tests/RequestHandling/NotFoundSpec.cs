926ec49386eb092b98c3c96ed80c2f1dd4d2a933
Jijie Chen <jijie.chen@someplace.com>
2016-01-14 21:43:03 +0800

    Use TestServer to serve a app for testing

--- test/Discussion.Web.Tests/RequestHandling/NotFoundSpec.cs
+++ test/Discussion.Web.Tests/RequestHandling/NotFoundSpec.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.AspNet.Builder.Internal;
-using Microsoft.AspNet.Hosting;
-using Microsoft.AspNet.Http.Internal;
-using Microsoft.Extensions.DependencyInjection;
-using Moq;
+﻿using Microsoft.AspNet.TestHost;
+using System.Net;
 using Xunit;
+
 
 namespace Discussion.Web.Tests.RequestHandling
 {
@@ -12,31 +10,16 @@ namespace Discussion.Web.Tests.RequestHandling
         [Fact]
         public async void should_response_not_found_by_default()
         {
-            // arrage
-            var services = new ServiceCollection();
-            services.AddLogging();
-            var startup = new Web.Startup();
-            startup.ConfigureServices(services);
-
-            var serviceProvider = services.BuildServiceProvider();
-            var app = new ApplicationBuilder(serviceProvider);
-            startup.Configure(app, (new Mock<IHostingEnvironment>()).Object);
+            // arrange
+            var server = new TestServer(TestServer.CreateBuilder()
+                   .UseStartup<Web.Startup>()
+                   .UseEnvironment("Production"));
 
             // act
-            var httpContext = new DefaultHttpContext();
-            httpContext.Request.Path = "/something-not-defined";
+            var response = await server.CreateRequest("/something-not-defined").GetAsync();
 
             // assert
-            httpContext.Response.StatusCode.ShouldEqual(200);
-
-
-
-            // act
-            await app.Build().Invoke(httpContext);
-
-            // assert
-            httpContext.Response.StatusCode.ShouldEqual(404);
-            // httpContext.Response.Body
+            response.StatusCode.ShouldEqual(HttpStatusCode.NotFound);
         }
     }
 }
