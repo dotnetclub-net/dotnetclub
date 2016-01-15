af0c9e90a6dc0e708eaa3d0296cd03f0aba99947
Jijie Chen <jijie.chen@someplace.com>
2016-01-15 22:56:16 +0800

    merged build-tools/init-env into sake

--- sake.sh
+++ sake.sh
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
+export DNX_FEED=https://www.nuget.org/api/v2
+build_dnx_version=1.0.0-rc1-update1
 
-mono build-tools/Sake/tools/Sake.exe -I KoreBuild/build "$@"
+if test ! -f ./.nuget/nuget.exe; then
+	if test ! -e .nuget; then
+		mkdir .nuget
+	fi
+    cp ./build-tools/nuget.exe ./.nuget/nuget.exe
+fi
+
+if ! type dnx > /dev/null 2>&1 || [ -z "$SKIP_DNX_INSTALL" ]; then
+	source ./build-tools/KoreBuild/build/dnvm.sh
+    dnvm install %build_dnx_version -p -runtime coreclr -alias default
+    dnvm install %build_dnx_version -p -runtime mono -alias default
+else
+	source ./build-tools/KoreBuild/build/dnvm.sh
+    dnvm use default -p -runtime mono
+fi
+
+mono ./build-tools/Sake/tools/Sake.exe -I KoreBuild/build "$@"
