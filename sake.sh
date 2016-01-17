3f5f406658c75004cf5b8a919253ec94cca41c9a
Jijie <jijie@someplace.com>
2016-01-17 10:54:20 +0800

    added execution mode to sake.sh; solved variable and path issue in sake.sh

--- sake.sh
+++ sake.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export DNX_FEED=https://www.nuget.org/api/v2
-build_dnx_version=1.0.0-rc1-update1
+build_dnx_version="1.0.0-rc1-update1"
 
 if test ! -f ./.nuget/nuget.exe; then
 	if test ! -e .nuget; then
@@ -11,10 +11,10 @@ fi
 
 if ! type dnx > /dev/null 2>&1 || [ -z "$SKIP_DNX_INSTALL" ]; then
 	source ./build-tools/KoreBuild/build/dnvm.sh
-    dnvm install %build_dnx_version -p -runtime coreclr -alias default
-    dnvm install %build_dnx_version -p -runtime mono -alias default
+    dnvm install $build_dnx_version -p -runtime coreclr -alias default
+    dnvm install $build_dnx_version -p -runtime mono -alias default
 else
-	source ./build-tools/KoreBuild/build/dnvm.sh
+    source ./build-tools/KoreBuild/build/dnvm.sh
     dnvm use default -p -runtime mono
 fi
 
