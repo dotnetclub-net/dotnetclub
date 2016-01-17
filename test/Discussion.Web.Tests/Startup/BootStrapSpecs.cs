0f032c67a20fc6147614911db40bbc1c5b8b57e1
Jijie Chen <jijie.chen@someplace.com>
2016-01-17 11:45:47 +0800

    changed the way to get dnxPath from environment PATH

--- test/Discussion.Web.Tests/Startup/BootStrapSpecs.cs
+++ test/Discussion.Web.Tests/Startup/BootStrapSpecs.cs
@@ -3,6 +3,8 @@ using System.Diagnostics;
 using System.IO;
 using Xunit;
 using System;
+using System.Linq;
+using Microsoft.AspNet.Testing;
 
 namespace Discussion.Web.Tests.Startup
 {
@@ -40,7 +42,7 @@ namespace Discussion.Web.Tests.Startup
         {
             var args = Environment.GetCommandLineArgs();
 
-            var dnxPath = args[0];
+            var dnxPath = DnxPath();
             var appBaseIndex = Array.IndexOf(args, "--appbase");
             var testPath = appBaseIndex >= 0 ? args[appBaseIndex + 1] : Environment.CurrentDirectory;
             var webProject = Path.Combine(testPath, "../../src/Discussion.Web").NormalizeSeparatorChars();
@@ -93,6 +95,24 @@ namespace Discussion.Web.Tests.Startup
             dnxWebServer.BeginErrorReadLine();
             dnxWebServer.BeginOutputReadLine();
             dnxWebServer.WaitForExit(20 * 1000);
+        }
+
+        private static string DnxPath()
+        {
+            var dnxPathFragment = string.Concat(".dnx", Path.DirectorySeparatorChar, "runtimes");
+            var dnxCommand = TestPlatformHelper.IsWindows ? "dnx.exe" : "dnx";
+            var envPathSeparator = TestPlatformHelper.IsWindows ? ';' : ':';
+
+            var envPath = Environment.GetEnvironmentVariable("PATH");            
+            var runtimeBin = envPath.Split(new char[] { envPathSeparator }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(c => c.Contains(dnxPathFragment)).FirstOrDefault();
+
+            if (string.IsNullOrWhiteSpace(runtimeBin))
+            {
+                throw new Exception("Runtime not detected on the machine.");
+            }
+
+            return Path.Combine(runtimeBin, dnxCommand);
         }
     }
 
