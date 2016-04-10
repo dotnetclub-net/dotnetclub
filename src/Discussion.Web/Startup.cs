cb0e16a73c97c191884ad81c6e0be24358c95718
Jijie Chen <jijie.chen@someplace.com>
2016-04-10 19:15:34 +0800

    When using Mono, use a synchronous FileStream instead of default asynchronous FileStream when reading Razor view files

--- src/Discussion.Web/Startup.cs
+++ src/Discussion.Web/Startup.cs
@@ -7,7 +7,14 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.PlatformAbstractions;
-
+using Microsoft.AspNet.Mvc.Razor.Compilation;
+using System;
+using Microsoft.Extensions.OptionsModel;
+using Microsoft.AspNet.Mvc.Razor;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.AspNet.FileProviders;
+using Microsoft.Extensions.Primitives;
+using System.IO;
 
 namespace Discussion.Web
 {
@@ -24,6 +31,12 @@ namespace Discussion.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddLogging();
+
+            if (IsMono())
+            {
+                Console.WriteLine("Replaced mvc default ICompilerCacheProvider by SyncFileProviderCompilerCacheProvider. \n Since Mono has a bug on asynchronous filestream.\n See https://github.com/aspnet/Hosting/issues/604");
+                services.TryAddSingleton<ICompilerCacheProvider, SyncFileProviderCompilerCacheProvider>();
+            }
             services.AddMvc();
 
 
@@ -49,7 +62,7 @@ namespace Discussion.Web
             }
             else
             {
-               app.UseExceptionHandler("/error");
+                app.UseExceptionHandler("/error");
             }
 
             // Add the platform handler to the request pipeline.
@@ -71,5 +84,134 @@ namespace Discussion.Web
             return builder.Build();
         }
 
+        static bool IsMono()
+        {
+            return Type.GetType("Mono.Runtime") != null;
+        }
+
     }
+
+    //class CoreReplacement : DefaultRazorViewEngineFileProviderAccessor
+    //{
+
+    //}
+
+
+    public class SyncFileProviderCompilerCacheProvider : ICompilerCacheProvider
+    {
+        public SyncFileProviderCompilerCacheProvider(IOptions<RazorViewEngineOptions> mvcViewOptions)
+        {
+            var fileProvider = new WrappedFileProvider(mvcViewOptions.Value.FileProvider);
+            Cache = new CompilerCache(fileProvider);
+        }
+
+        public ICompilerCache Cache { get; }
+
+
+        public class WrappedFileProvider : IFileProvider
+        {
+            IFileProvider _original;
+            public WrappedFileProvider(IFileProvider original)
+            {
+                _original = original;
+            }
+
+
+            public IDirectoryContents GetDirectoryContents(string subpath)
+            {
+                return _original.GetDirectoryContents(subpath);
+            }
+
+            public IFileInfo GetFileInfo(string subpath)
+            {
+                var originalFileInfo = _original.GetFileInfo(subpath);
+                var isPhysical = originalFileInfo.GetType().FullName.EndsWith("PhysicalFileInfo");
+                if (!isPhysical)
+                {
+                    return originalFileInfo;
+                }
+
+                return new WrappedSynchronousFileInfo(originalFileInfo);
+            }
+
+            public IChangeToken Watch(string filter)
+            {
+                return _original.Watch(filter);
+            }
+
+
+            public class WrappedSynchronousFileInfo : IFileInfo
+            {
+                IFileInfo _original;
+                public WrappedSynchronousFileInfo(IFileInfo original)
+                {
+                    _original = original;
+                }
+
+
+                public bool Exists
+                {
+                    get
+                    {
+                        return _original.Exists;
+                    }
+                }
+
+                public bool IsDirectory
+                {
+                    get
+                    {
+                        return _original.IsDirectory;
+                    }
+                }
+
+                public DateTimeOffset LastModified
+                {
+                    get
+                    {
+                        return _original.LastModified;
+                    }
+                }
+
+                public long Length
+                {
+                    get
+                    {
+                        return _original.Length;
+                    }
+                }
+
+                public string Name
+                {
+                    get
+                    {
+                        return _original.Name;
+                    }
+                }
+
+                public string PhysicalPath
+                {
+                    get
+                    {
+                        return _original.PhysicalPath;
+                    }
+                }
+
+                public Stream CreateReadStream()
+                {
+                    // Note: Buffer size must be greater than zero, even if the file size is zero.
+                    return new FileStream(
+                        PhysicalPath,
+                        FileMode.Open,
+                        FileAccess.Read,
+                        FileShare.ReadWrite,
+                        1024 * 64,
+                        FileOptions.SequentialScan);
+                }
+            }
+        }
+    }
+
+
+
 }
