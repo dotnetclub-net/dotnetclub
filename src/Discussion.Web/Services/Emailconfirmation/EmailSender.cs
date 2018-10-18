9ce55c9c19bb643917b703ba08166463e8130af4
lmc <313801700@qq.com>
2018-10-19 00:06:21 +0800

    add emailsender

---
+++ src/Discussion.Web/Services/Emailconfirmation/EmailSender.cs
@@ -0,0 +1,41 @@
+﻿using Discussion.Web.Infrastructure;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Discussion.Web.Services.Emailconfirmation
+{
+    public class EmailSender : IEmailSender
+    {
+        public AuthMessageSenderOptions Options { get; } //set only via Secret Manager
+        public EmailSender(IOptions<AuthMessageSenderOptions> optionsAccessor)
+        {
+            Options = optionsAccessor.Value;
+        }
+        public Task SendEmailAsync(string email, string subject, string message)
+        {
+            return Execute(Options.SendGridKey, subject, message, email);
+        }
+        public Task Execute(string apiKey,string subject,string message,string email)
+        {
+            var client = new SendGridClient(apiKey);
+            var msg = new SendGridMessage()
+            {
+                From = new EmailAddress("Joe@contoso.com", "Joe Smith"),
+                Subject = subject,
+                PlainTextContent = message,
+                HtmlContent = message
+            };
+            msg.AddTo(new EmailAddress(email));
+            msg.TrackingSettings = new TrackingSettings
+            {
+                ClickTracking = new ClickTracking { Enable = false }
+            };
+            return client.SendEmailAsync(msg);
+        }
+    }
+}
