2855a5de1a6bfa1446d4c8627cda59c8f7ea1a8a
Jijie Chen <jijie.chen@someplace.com>
2019-01-09 22:11:47 +0800

    在 Admin 站点中，添加了显示话题列表，并能删除话题

--- src/Discussion.Admin/ClientApp/src/app/routes/topic-management/topic-list.component.ts
+++ src/Discussion.Admin/ClientApp/src/app/routes/topic-management/topic-list.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import { _HttpClient } from '@delon/theme';
-import {Topic} from './topic';
-
+import {Paged, Paging, TopicSummary} from './topic';
+import {ApiResponse} from "../../api-response";
+import {STChange, STColumn, STPage} from "@delon/abc";
+import {NzMessageService} from "ng-zorro-antd";
 
 
 @Component({
@@ -11,20 +13,93 @@ import {Topic} from './topic';
 
 
 export class TopicListComponent implements OnInit {
-  topics: Array<Topic>;
-  constructor(private httpClient: _HttpClient) { }
+  dotnetClubHostName: string = "localhost:5021";
+
+  topics: TopicSummary[] = [];
+  paging: Paging = new Paging();
+  loading: boolean = false;
+  error: any = null;
+
+  stPagingOptions: STPage = {
+    front: false
+  };
+  columns: STColumn[] = [
+    { title: '编号', index: 'id', width: '5%',
+      format: (topic: TopicSummary) => {
+        return `<a href="https://${this.dotnetClubHostName}/topics/${topic.id}" target="_blank">${topic.id}</a>`
+      }
+    },
+    { title: '标题', index: 'title', width: '60%'},
+    {
+      title: '作者',
+      index: 'author.displayName',
+      width: '20%'
+    },
+    {
+      title: '操作',
+      width: '15%',
+      buttons: [
+        {
+          text: '删除',
+          click: (topic: TopicSummary) => {
+            if(!window.confirm(`确实要删除话题 ${topic.title} 吗？`)){
+              return;
+            }
+
+            this.delete(topic.id, topic.title);
+          }
+        }
+      ],
+    },
+  ];
+
+  constructor(private httpClient: _HttpClient, private msg: NzMessageService) { }
+
+  listChange( event: STChange){
+    if(event.pi !== this.paging.currentPage){
+      this.getTopics(event.pi);
+    }
+  }
 
 
+  getTopics(page : number){
+    this.error = null;
+    this.loading = true;
 
+    this.httpClient.get<ApiResponse>('api/topics?page=' + page)
+      .subscribe(data => {
+        this.loading = false;
+
+        if(data.code === 200){
+          const topicResult = <Paged<TopicSummary>>data.result;
+
+          this.topics = topicResult.items;
+          this.paging = topicResult.paging;
+        }else{
+          this.msg.error(data.errorMessage);
+        }
+      }, (err: any) => {
+        this.loading = false;
+        this.msg.error(err);
+      });
+  }
+
+  delete(topicId: number, topicTitle: string){
+    this.httpClient.delete('api/topics/' + topicId)
+      .subscribe((data: ApiResponse) => {
+        if(data.code === 200){
+          this.msg.success(`已删除 ${topicTitle}`);
+          this.getTopics(0);
+        }else{
+          this.msg.error(data.errorMessage);
+        }
+      }, (err: any) => {
+        this.msg.error(err);
+      });
+  }
 
   ngOnInit() {
-    this.httpClient.get<Array<Topic>>('api/topics')
-                   .subscribe(data => { this.topics = data; });
-
+    this.getTopics(1);
   }
 
 }
-
-
-
-
--- src/Discussion.Admin/ClientApp/src/app/routes/topic-management/topic.ts
+++ src/Discussion.Admin/ClientApp/src/app/routes/topic-management/topic.ts
@@ -3,3 +3,33 @@ export class Topic {
   title: string;
   content: string;
 }
+
+export class TopicSummary {
+  id: number;
+  title: string;
+  createdAt: Date;
+  author: TopicAuthorSummary;
+}
+
+export class TopicAuthorSummary {
+  id: number;
+  displayName: string;
+}
+
+
+export class Paged<T> {
+  items: T[];
+  paging: Paging;
+}
+
+export class Paging {
+  pageSize: number = 20;
+  itemCount: number = 0;
+  currentPage: number = 1;
+  totalPages: number = 0;
+
+  hasNextPage: boolean = false;
+  hasPreviousPage: boolean = false;
+}
+
+
