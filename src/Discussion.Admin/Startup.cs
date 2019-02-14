97025fe51c560f04756981ab14f5822754e0b496
Jijie Chen <jijie.chen@someplace.com>
2019-02-14 18:13:16 +0800

    让 Admin 网站可以向外提供 SPA 所需的静态文件服务

--- src/Discussion.Admin/Startup.cs
+++ src/Discussion.Admin/Startup.cs
@@ -70,6 +70,8 @@ namespace Discussion.Admin
             app.UseApiResponse();
             app.UseAuthentication();
             app.UseMvc();
+            
+            app.UseSpaStaticFiles();
             app.UseSpa(spa =>
             {
                 // To learn more about options for serving an Angular SPA from ASP.NET Core, see https://go.microsoft.com/fwlink/?linkid=864501
